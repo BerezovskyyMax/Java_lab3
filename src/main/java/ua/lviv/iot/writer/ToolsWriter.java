784194403e90d51c6ad2cd21d66ea85c7de9873b
BerezovskyyMax <berezovskyymax@gmail.com>
2020-03-06 20:13:01 +0200

    added writter to another package

---
+++ src/main/java/ua/lviv/iot/writer/ToolsWriter.java
@@ -0,0 +1,33 @@
+package ua.lviv.iot.writer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import ua.lviv.iot.model.AbstractWateringTool;
+
+public class ToolsWriter {
+
+
+    public void writeToFile(List<AbstractWateringTool> tools) {
+
+        String pathToFile = new File("").getAbsolutePath();
+
+        File file = new File(pathToFile + "//test.txt");
+
+        try(Writer writer = new FileWriter(file, StandardCharsets.UTF_8)) {
+
+            for (AbstractWateringTool currentTool : tools) {
+                writer.write(currentTool.getHeaders() + currentTool.toCSV() + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
